1eac3702562fade6da0b850eb85e4dd07dd4fbb7
cowtongue <revisedboomerguy@gmail.com>
2016-01-23 05:21:06 -0600

    added tv

---
+++ Assets/Resources/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovieController : MonoBehaviour {
+
+    public MovieTexture movieTexture;
+    private AudioSource aud;
+    // Use this for initialization
+    void Start () {
+        AudioSource aud = GetComponent<AudioSource>();
+        aud.clip = movieTexture.audioClip;
+        movieTexture.Play();
+        aud.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
