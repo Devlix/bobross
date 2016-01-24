f35bd26c17e05da2ac786e109bee624c0f289a14
cowtongue <revisedboomerguy@gmail.com>
2016-01-23 22:19:21 -0600

    Added Skip for Splash

---
+++ Assets/SkipSplash.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SkipSplash : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Z))
+	    {
+            Debug.Log("New Level load: 01a Start");
+            Application.LoadLevel("01a Start");
+        }
+	}
+}
